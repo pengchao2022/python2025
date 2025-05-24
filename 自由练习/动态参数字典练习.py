031f2c83e0977695b71cf0685104d83b8e84ced8
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-05-24 16:48:23 +0800

    added

--- 自由练习/动态参数字典练习.py
+++ 自由练习/动态参数字典练习.py
@@ -3,3 +3,5 @@ def stu_info(**kwargs):
 
 
 stu_info(name='allen', age=37) #打印结果是字典 {'name': 'allen', 'age': 37}
+
+stu_info(name='kate', age=40, height=180)
---
+++ 自由练习/时间戳练习1.py
@@ -0,0 +1,25 @@
+import time
+
+#时间戳从1970年1月1日0时0分0秒开始到现在
+#print(time.time())
+
+#时间元组
+t = time.time()
+#print(time.gmtime(t))
+
+#格式化时间
+ftime = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(t))
+print(f"当前时间为：{ftime}") #时区有问题 gmtime 返回的是0时区
+
+#使用localtie 打印本地时间
+f_local_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(t))
+print(f"本地时间为：{f_local_time}")
+
+
+#相互转换 具体时间转换为元组
+str_t = '2025-5-23 14:30:20'
+#转换时候使用 strptime函数
+#print(time.strptime(str_t, '%Y-%m-%d %H:%M:%S'))
+
+#带有星期的时间
+print(time.asctime(time.localtime(t)))
